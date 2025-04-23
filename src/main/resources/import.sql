c0670b81d8c0d806426f41c0b08626c1eb2013db
leonarolivei <leonardo.noliveira@mercadolivre.com>
2025-04-23 14:20:03 -0300

    feat: add initial schema

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,50 @@
--- This file allow to write SQL commands that will be emitted in test and dev.
--- The commands are commented as their support depends of the database
--- insert into myentity (id, field) values(1, 'field-1');
--- insert into myentity (id, field) values(2, 'field-2');
--- insert into myentity (id, field) values(3, 'field-3');
--- alter sequence myentity_seq restart with 4;
+drop table if exists candidate cascade
+
+drop table if exists election cascade
+
+drop table if exists politician cascade
+
+drop sequence if exists candidate_SEQ
+
+drop sequence if exists election_SEQ
+
+drop sequence if exists politician_SEQ
+
+create sequence candidate_SEQ start with 1 increment by 50
+
+create sequence election_SEQ start with 1 increment by 50
+
+create sequence politician_SEQ start with 1 increment by 50
+
+create table candidate
+(
+    electionId   integer not null,
+    number       integer not null,
+    politicianId integer not null,
+    votes        integer not null,
+    id           bigint  not null,
+    result       varchar(255) check (result in ('SECOND_TURN', 'NOT_ELECTED', 'ELECTED')),
+    primary key (id),
+    unique (politicianId, electionId)
+)
+
+create table election
+(
+    id           bigint not null,
+    municipality varchar(255) check (municipality in ('SAO_PAULO')),
+    position     varchar(255) check (position in ('PREFEITO', 'VEREADOR')),
+    uf           varchar(255) check (uf in
+                                     ('AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MG', 'MS', 'MT',
+                                      'PA', 'PB', 'PE', 'PI', 'PR', 'RJ', 'RN', 'RO', 'RR', 'RS', 'SC', 'SE', 'SP',
+                                      'TO')),
+    primary key (id)
+)
+
+create table politician
+(
+    id        bigint not null,
+    full_name varchar(255),
+    name      varchar(255),
+    photo     varchar(255),
+    primary key (id)
+)
